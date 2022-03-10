259c9dc078df243767c093a0014f6472df964d1a
luis <lualreye@gmail.com>
2022-03-10 17:59:58 -0500

    add: background image added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <router-view></router-view>
+  <main id="app" class="w-screen h-screen" :style="{backgroundImage: 'url(' + bgImage + ')'}" >
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
 export default {
   name: 'App',
+  setup() {
+    const bgImage = require("./assets/images/svg/blob-scene-haikei.svg")
+
+
+    return {
+      bgImage
+    }
+  }
 }
 </script>
+
+<style scoped>
+#app {
+  background-size: cover;
+  background-origin: center;
+  background-repeat: no-repeat;
+}
+</style>
