b06024a1f7e872080241bb039d4ebf6ab8d13e49
luis <lualreye@gmail.com>
2022-03-10 17:59:16 -0500

    feat: customizing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundWhite: "#E5E5E5",
+        backgroundLightBlue: "#7FC0FF",
+        primary: "#3970A6",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
