bc7c127c8ea45e9dec1e4e1d038a7e7a96cbe18e
luis <lualreye@gmail.com>
2022-03-10 17:39:31 -0500

    feat: setting main.js up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router"
+import store from "./store/index"
+import "./assets/css/reset.css"
+import "./assets/css/tailwind.css"
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(store)
+
+
+app.mount('#app')
