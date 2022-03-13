51dc26a1f0758a41b0f3cf4b7741023e1032506e
luis <lualreye@gmail.com>
2022-03-13 15:48:53 -0500

    feat: global component for icons created

---
+++ src/components/Global/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    class="select-none"
+    style="width: 100%; height: 100%; stroke-width: 2.5px"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path :d="icon" />
+  </svg>
+</template>
+
+<script>
+import Icons from "../../helpers/Icons.js";
+import { computed } from "vue";
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const icon = computed(() => {
+      return Icons[props.name];
+    });
+
+    return {
+      icon,
+    };
+  },
+};
+</script>
