d0a6b3b418981f8fa71075d0a6b3d9a5428a0529
luis <lualreye@gmail.com>
2022-03-13 17:35:09 -0500

    feat: global button added to the project

---
+++ src/components/Global/CButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    class="w-56 py-3 text-xs flex justify-center items-center bg-blue text-backgroundWhite rounded-lg"
+    :class="buttonDisabled"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    {{ name }}
+  </button>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props) {
+    const buttonDisabled = computed(() => {
+      if (props.disabled) {
+        return "bg-blue-300";
+      } else {
+        return "bg-primary";
+      }
+    });
+
+    return {
+      buttonDisabled,
+    };
+  },
+};
+</script>
