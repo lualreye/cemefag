0c2b98ed66c36c2bef455355f012186dd0b15f6d
luis <lualreye@gmail.com>
2022-03-13 17:36:01 -0500

    feat: inputs validation added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen flex justify-center items-center">
     <div class="w-56">
       <h1 class="w-full text-center text-3xl text-textColor mb-20">Registro</h1>
-      <div class="w-full flex flex-col justify-center items-center">
+      <form class="w-full flex flex-col justify-center items-center">
         <div class="w-full flex flex-col justify-center items-center mb-4">
           <!-- USER INPUT -->
           <label class="text-sm text-textColor w-full text-left mb-1">
@@ -16,10 +16,14 @@
             </div>
             <input
               type="email"
+              v-model="user"
               placeholder="Escriba su usuario"
               class="placeholder-grayColor bg-backgroundWhite border-none focus:border-none outline-none text-primary w-full text-xs"
             />
           </div>
+          <span v-if="emailValidation" class="text-xs font-light text-pink-600"
+            >Email no valido</span
+          >
           <!-- PASSWORD INPUT -->
           <label class="text-sm text-textColor w-full text-left mb-1 mt-4">
             Contraseña
@@ -32,12 +36,20 @@
             </div>
             <input
               type="password"
+              v-model="password"
               placeholder="Escriba su contraseña"
               class="placeholder-grayColor bg-backgroundWhite border-none focus:border-none outline-none text-primary w-full text-xs"
             />
           </div>
+          <span
+            v-if="passwordValidation"
+            class="text-xs font-light text-pink-600"
+            >No haz introducido password</span
+          >
         </div>
-        <div class="w-full flex flex-col justify-center items-center"></div>
+      </form>
+      <div class="w-full flex justify-center items-center">
+        <c-button name="Ingresar" :disabled="credentialsAreValid" @click="sendCredentials" />
       </div>
     </div>
   </div>
@@ -45,9 +57,48 @@
 
 <script>
 import Icon from "../components/Global/Icon.vue";
+import CButton from "../components/Global/CButton.vue";
+import { computed, reactive, toRefs } from "vue";
 export default {
   components: {
     Icon,
+    CButton,
+  },
+  setup() {
+    const credentials = reactive({
+      user: null,
+      password: null,
+    });
+
+    const emailValidation = computed(() => {
+      return emailisValid(credentials.user) || credentials.user === null ? false : true;
+    });
+
+    function emailisValid(email) {
+      const userRgx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+      return userRgx.test(email);
+    }
+
+    const passwordValidation = computed(() => {
+      return credentials.password === "" ? true : false;
+    });
+
+    const credentialsAreValid = computed(() => {
+      return emailValidation.value && passwordValidation.value;
+    });
+
+
+    function sendCredentials() {
+      console.log(credentials.user, credentials.password)
+    }
+
+    return {
+      ...toRefs(credentials),
+      emailValidation,
+      passwordValidation,
+      credentialsAreValid,
+      sendCredentials
+    };
   },
 };
 </script>
