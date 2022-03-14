cad0156ad76bd719ad11be5086cd4db88a131ebf
luis <lualreye@gmail.com>
2022-03-14 02:19:21 -0500

    feat: userAuthentication module added to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import { createStore } from "vuex";
+import userAuthentication from "./modules/userAuthentication";
 
-const store = createStore({});
+const store = createStore({
+  modules: {
+    userAuthentication
+  }
+});
 
 export default store;
