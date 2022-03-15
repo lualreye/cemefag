6d54c7a55e2fb47794d4cce155bf319a4b39b1b5
luis <lualreye@gmail.com>
2022-03-15 17:10:21 -0500

    feat: edit and create consultation

---
+++ src/components/CreateConsultation/NewConsultation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full">
+    <div
+      class="w-full flex border-b border-primary justify-between items-center px-1"
+    >
+      <p class="text-textColor text-sm font-light text-left">
+        AGENDAMIENTO DE CITA
+      </p>
+    </div>
+    <div class="w-full flex justify-center items-center my-14">
+      Aqui deben subir su cita
+    </div>
+  </div>
+</template>
