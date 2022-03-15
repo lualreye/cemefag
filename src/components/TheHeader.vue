d040f847e42930c20ca950ef30817b49ff710886
luis <lualreye@gmail.com>
2022-03-14 20:45:50 -0500

    fix: user modal working properly, prepared to sign out user

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,30 @@
 <template>
-  <header class="w-full flex justify-center items-center">
+  <header class="w-full flex justify-center items-center bg-backgroundWhite">
     <div
       class="max-w-screen-2xl w-full border-b border-primary flex items-center justify-between py-3 px-3"
     >
       <figure class="w-36 justify-center">
         <img :src="logo" alt="" />
       </figure>
-      <div class="flex justify-center items-center">
+      <div class="flex justify-center items-center relative">
+        <div
+          v-if="isModalOpen"
+          class="bg-backgroundWhite absolute right-0 top-full w-56 flex flex-col justify-center items-center shadow-lg p-4 divide-y divide-grayColor"
+        >
+          <div class="mb-2">
+            <router-link to="/user" class="text-base text-textColor">
+              Identificar nuevo paciente
+            </router-link>
+          </div>
+          <div class="w-full">
+            <button
+              class="w-full py-2 rounded-lg bg-pink-600 text-backgroundWhite text-sm font-light mt-4"
+              @click="logout"
+            >
+              Cerrar Sesion
+            </button>
+          </div>
+        </div>
         <div class="w-5 h-5 flex justify-center items-center mr-2">
           <icon name="account" class="text-textColor" />
         </div>
@@ -26,6 +44,7 @@
 import Icon from "@/components/Global/Icon.vue";
 import { useStore } from "vuex";
 import { computed, reactive, toRefs } from "vue";
+import { useRouter } from 'vue-router'
 export default {
   components: {
     Icon,
@@ -52,12 +71,19 @@ export default {
       }
     }
 
+
+    const router = useRouter()
+    function logout() {
+      router.push('/')
+    }
+
     return {
       user,
       ...toRefs(modalState),
       activeModal,
       arrowDirection,
       logo,
+      logout
     };
   },
 };
