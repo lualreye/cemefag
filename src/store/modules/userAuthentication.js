42bb75b5aae987d7e8b264fe6377830b0f672052
luis <lualreye@gmail.com>
2022-03-15 03:19:19 -0500

    feat: actions and getters added to create and get patients

--- src/store/modules/userAuthentication.js
+++ src/store/modules/userAuthentication.js
@@ -1,18 +1,50 @@
 const state = () => ({
-  user: 'Luis'
-})
+  user: "Luis",
+  // patient: null,
+  isLoading: false,
+  patient: {
+    name: "Luis",
+    lastname: "Reyes",
+    ci: "0924756018",
+    birthday: "1995-01-27",
+    email: "lualreye@gmail.com",
+    phone: "0960952970",
+  },
+});
 
 const getters = {
   getUser(state) {
-    return state.user
-  }
-}
+    return state.user;
+  },
+  getPatient(state) {
+    return state.patient;
+  },
+  getLoading(state) {
+    return state.isLoading;
+  },
+};
 
+const mutations = {
+  SET_PATIENT(state, patient) {
+    state.patient = patient;
+  },
+  SET_LOADING(state, boolean) {
+    state.isLoading = boolean;
+  },
+};
 
-
+const actions = {
+  async createPatientUp({ commit }, payload) {
+    commit("SET_LOADING", true);
+    commit("SET_PATIENT", payload);
+    commit("SET_LOADING", false);
+  },
+};
 
 export default {
   namespaced: true,
   state,
-  getters
-}
+  getters,
+  mutations,
+  actions,
+};
