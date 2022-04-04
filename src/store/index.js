1f7ec927fa7d4a6b8807d7067df03f0cd1934e16
lualreye <lualreye@gmail.com>
2022-04-04 13:37:03 -0500

    add: module added

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 import userAuthentication from "./modules/userAuthentication";
 import insurance from "./modules/insurance"
+import patient from "./modules/patient"
 
 const store = createStore({
   modules: {
     userAuthentication,
-    insurance
+    insurance,
+    patient
   }
 });
 
