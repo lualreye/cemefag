0b8b1ec8de7449c4858cac36195d128ba56f0882
lualreye <lualreye@gmail.com>
2022-04-09 00:02:12 -0500

    created: store for appointment

---
+++ src/store/modules/appointment.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const state = () => ({
+  specialities: {},
+});
+
+const getters = {
+  getSpecialities(state) {
+    return state.specialities;
+  },
+};
+
+const mutations = {
+  SET_SPECIALITIES(state, specialities) {
+    state.specialities = specialities;
+  },
+};
+
+const actions = {
+  async fetchSpecialities({ commit }) {
+    try {
+      const specialitiesResponse = await axios.get(
+        `${process.env.VUE_APP_BASE_URL}/speciality`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("user-token")}`,
+          },
+        }
+      );
+      commit("SET_SPECIALITIES", specialitiesResponse);
+    } catch (err) {
+      console.error("CANNOT_GET_SPECIALITIES", err);
+    }
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
