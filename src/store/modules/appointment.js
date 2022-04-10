f82ef5955bec700bb63f71ef60ae83aa3941c246
lualreye <lualreye@gmail.com>
2022-04-09 19:35:45 -0500

    feat: getting doctors rel speciality

--- src/store/modules/appointment.js
+++ src/store/modules/appointment.js
@@ -2,17 +2,27 @@ import axios from "axios";
 
 const state = () => ({
   specialities: [],
+  doctors: [],
 });
 
 const getters = {
   getSpecialities(state) {
     return state.specialities;
   },
+  getDoctors(state) {
+    return state.doctors;
+  },
 };
 
 const mutations = {
   SET_SPECIALITIES(state, specialities) {
     state.specialities = specialities;
+  },
+  SET_DOCTORS(state, doctors) {
+    state.doctors = doctors;
+  },
+  RESET_DOCTORS(state) {
+    state.doctors = [];
   },
 };
 
@@ -30,6 +40,21 @@ const actions = {
       commit("SET_SPECIALITIES", specialitiesResponse.data.data);
     } catch (err) {
       console.error("CANNOT_GET_SPECIALITIES", err);
+    }
+  },
+  async fetchDoctors({ commit }, payload) {
+    try {
+      const doctorsResponses = await axios.get(
+        `${process.env.VUE_APP_BASE_URL}/speciality/${payload}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("user-token")}`,
+          },
+        }
+      );
+      commit("SET_DOCTORS", doctorsResponses.data.doctors);
+    } catch (err) {
+      console.error("CANNOT_GET_DOCTORS", err);
     }
   },
 };
